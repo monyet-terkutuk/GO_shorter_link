14af1322896e7856d5ac170e7545c39074d96afd
undeath-cyber <gabrielxjonathan.12@gmail.com>
2023-08-28 08:30:33 +0700

    udah dlu

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,6 +1,11 @@
 package routes
 
-import "time"
+import (
+	"shorter_link/helpers"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 type request struct {
 	URL         string        `json:"url"`
@@ -14,4 +19,26 @@ type response struct {
 	Expiry         time.Duration `json:"expiry"`
 	XRateRemaining int           `json:"rate_limit"`
 	XRateLimitRest time.Duration `json:"rate_limit_reset"`
+}
+
+func ShortenURL(c *fiber.Ctx) error {
+	body := new(request)
+
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "cannot parse JSON"})
+	}
+
+	// implementasi rate limit
+
+	// cek input user
+	if !govalidator.IsURL(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid URL!"})
+	}
+
+	// check domain
+	if !helpers.RemoveDomainError(body.URL) {
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error": "sadasd"})
+	}
+	// enforce https,ssl
+	body.URL = helpers.EnforceHTTP(body.URL)
 }
